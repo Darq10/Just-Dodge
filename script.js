365e6568ecb92ddd535c6d18784bb57887b0b982
maks-darqness <76443059+Darqlikeyou10@users.noreply.github.com>
2022-12-10 19:32:57 +0100

    Better Rocket
    You can move the rocket by W and S and it has animation

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 window.addEventListener('keydown', function (e) {
 
-    if(e.key=='w')
+    if(e.key=='w' && this.document.getElementById("rocketPart").classList.contains("h2d") || e.key=='w' && this.document.getElementById("rocketPart").classList.contains("h2u"))
     {
-        this.document.getElementById("rocketPart").classList.remove("h2");
+        this.document.getElementById("rocketPart").classList.remove("h1");
         this.document.getElementById("rocketPart").classList.remove("h3");
+        this.document.getElementById("rocketPart").classList.remove("h2d");
+        this.document.getElementById("rocketPart").classList.remove("h2u");
         this.document.getElementById("rocketPart").classList.add("h1");
-        this.document.getElementById("firePart").classList.remove("h2");
+        this.document.getElementById("firePart").classList.remove("h1");
         this.document.getElementById("firePart").classList.remove("h3");
+        this.document.getElementById("firePart").classList.remove("h2u");
+        this.document.getElementById("firePart").classList.remove("h2d");
         this.document.getElementById("firePart").classList.add("h1");
     }
-    else if(e.key=='s')
-    {
-        this.document.getElementById("rocketPart").classList.remove("h1");
-        this.document.getElementById("rocketPart").classList.remove("h2");
-        this.document.getElementById("rocketPart").classList.add("h3");
-        this.document.getElementById("firePart").classList.remove("h1");
-        this.document.getElementById("firePart").classList.remove("h2");
-        this.document.getElementById("firePart").classList.add("h3");
-    }
-    else if(e.key==" ")
+    if(e.key=='w' && this.document.getElementById("rocketPart").classList.contains("h3"))
     {
         this.document.getElementById("rocketPart").classList.remove("h1");
         this.document.getElementById("rocketPart").classList.remove("h3");
-        this.document.getElementById("rocketPart").classList.add("h2");
+        this.document.getElementById("rocketPart").classList.remove("h2d");
+        this.document.getElementById("rocketPart").classList.remove("h2u");
+        this.document.getElementById("rocketPart").classList.add("h2u");
         this.document.getElementById("firePart").classList.remove("h1");
         this.document.getElementById("firePart").classList.remove("h3");
-        this.document.getElementById("firePart").classList.add("h2");
+        this.document.getElementById("firePart").classList.remove("h2u");
+        this.document.getElementById("firePart").classList.remove("h2d");
+        this.document.getElementById("firePart").classList.add("h2u");
+    }
+    else if(e.key=='s' && this.document.getElementById("rocketPart").classList.contains("h2d") || e.key=='s' && this.document.getElementById("rocketPart").classList.contains("h2u"))
+    {
+        this.document.getElementById("rocketPart").classList.remove("h1");
+        this.document.getElementById("rocketPart").classList.remove("h3");
+        this.document.getElementById("rocketPart").classList.remove("h2d");
+        this.document.getElementById("rocketPart").classList.remove("h2u");
+        this.document.getElementById("rocketPart").classList.add("h3");
+        this.document.getElementById("firePart").classList.remove("h1");
+        this.document.getElementById("firePart").classList.remove("h3");
+        this.document.getElementById("firePart").classList.remove("h2u");
+        this.document.getElementById("firePart").classList.remove("h2d");
+        this.document.getElementById("firePart").classList.add("h3");
+    }    
+    else if(e.key=='s' && this.document.getElementById("rocketPart").classList.contains("h1"))
+    {
+        this.document.getElementById("rocketPart").classList.remove("h1");
+        this.document.getElementById("rocketPart").classList.remove("h3");
+        this.document.getElementById("rocketPart").classList.remove("h2d");
+        this.document.getElementById("rocketPart").classList.remove("h2u");
+        this.document.getElementById("rocketPart").classList.add("h2d");
+        this.document.getElementById("firePart").classList.remove("h1");
+        this.document.getElementById("firePart").classList.remove("h3");
+        this.document.getElementById("firePart").classList.remove("h2u");
+        this.document.getElementById("firePart").classList.remove("h2d");
+        this.document.getElementById("firePart").classList.add("h2d");
     }
 
   }, false);
